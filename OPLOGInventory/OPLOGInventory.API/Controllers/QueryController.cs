97498682b8e7065821ead9d6780ba14c21080ce2
Serkan İnce <serkan.ince@arvento.com>
2020-07-20 13:35:13 +0300

    bug fix

--- OPLOGInventory/OPLOGInventory.API/Controllers/QueryController.cs
+++ OPLOGInventory/OPLOGInventory.API/Controllers/QueryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OPLOGInventory.Application.QueryApplications;
@@ -12,6 +13,7 @@ namespace OPLOGInventory.API.Controllers
 {
     [Route("api/v1/query")]
     [ApiController]
+    [Authorize]
     public class QueryController : ControllerBase
     {
         public QueryController(IQueryApplication queryApplication)
